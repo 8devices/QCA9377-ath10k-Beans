0fc64d5b2008ff505b167109792e5cbd67dfbd46
Tomas C <tomas.c@8devices.com>
2025-03-31 08:53:41 +0000

    Add board data files for QCA9377-7

--- package/firmware/linux-firmware/qca_ath10k.mk
+++ package/firmware/linux-firmware/qca_ath10k.mk
@@ -18,17 +18,22 @@ $(eval $(call BuildPackage,ath10k-firmware-qca4019))
 Package/ath10k-board-qca9377 = $(call Package/firmware-default,ath10k qca9377 board firmware,,LICENSE.QualcommAtheros_ath10k)
 define Package/ath10k-board-qca9377/install
 	$(INSTALL_DIR) $(1)/lib/firmware/ath10k/QCA9377/hw1.0
+	$(INSTALL_DIR) $(1)/lib/firmware/ath10k/QCA9377/hw1.1
 	$(INSTALL_DATA) \
 		$(PKG_BUILD_DIR)/ath10k/QCA9377/hw1.0/board-2.bin \
 		$(1)/lib/firmware/ath10k/QCA9377/hw1.0/
+	$(CP) ./files/*.bin $(1)/lib/firmware/ath10k/QCA9377/hw1.1/
 endef
 $(eval $(call BuildPackage,ath10k-board-qca9377))
 Package/ath10k-firmware-qca9377 = $(call Package/firmware-default,ath10k qca9377 firmware,+ath10k-board-qca9377,LICENSE.QualcommAtheros_ath10k)
 define Package/ath10k-firmware-qca9377/install
 	$(INSTALL_DIR) $(1)/lib/firmware/ath10k/QCA9377/hw1.0
+	$(INSTALL_DIR) $(1)/lib/firmware/ath10k/QCA9377/hw1.1
 	$(INSTALL_DATA) \
 		$(PKG_BUILD_DIR)/ath10k/QCA9377/hw1.0/firmware-6.bin \
 		$(1)/lib/firmware/ath10k/QCA9377/hw1.0/firmware-6.bin
+	$(CP) $(PKG_BUILD_DIR)/ath10k/QCA9377/hw1.0/firmware-sdio-5.bin $(1)/lib/firmware/ath10k/QCA9377/hw1.1/firmware-sdio-5.bin
+	$(CP) $(PKG_BUILD_DIR)/ath10k/QCA9377/hw1.0/firmware-sdio-5.bin $(1)/lib/firmware/ath10k/QCA9377/hw1.1/firmware-usb-5.bin
 endef
 $(eval $(call BuildPackage,ath10k-firmware-qca9377))
 
